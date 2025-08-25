439534e7f0d3b808b02ff299450156656938495a
sujeet yadav <sujityadav595@gmail.com>
2025-08-25 11:35:37 +0530

    blank commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../Hero-section'
+import About from "../About"
 
 const Home = () => {
   return (
     <div>
       <HeroSection/>
+      <About/>
       
     </div>
   )
